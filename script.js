732674e62bbd3950cf8510e0f8b9845081e9e9e5
Hazik Khatri <142565556+hazikkhatri@users.noreply.github.com>
2024-06-23 10:08:14 +0500

    script.js updated

--- script.js
+++ script.js
@@ -13,11 +13,23 @@ document.getElementById("searchBtn").addEventListener("click", (e) => {
 
 })
 
+let imageContainer = document.getElementById("imageContainer")
 const getPhotos = (query) => {
     let url = 'https://api.pexels.com/v1/search?query=' + query + "&per_page=15&page=1";
     fetch(url, options)
         .then(response => response.json())
         .then((response) => {
             console.log(response);
+            imageContainer.innerHTML = '';
+            response.photos.forEach(photo => {
+                let image = document.createElement('img')
+                image.src = photo.src.original;
+                image.width = 400;
+                image.height = 300;
+                imageContainer.appendChild(image);
+            });
+        })
+        .catch(err => {
+            console.log(err);
         })
 }
